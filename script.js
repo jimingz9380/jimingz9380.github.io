664d40c5b8aadaa8f02914a216442697d346d407
jimingz9380 <91745086+jimingz9380@users.noreply.github.com>
2021-10-01 11:16:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = " Jiming  Zheng"
+let userName = "Jimingz9380";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
